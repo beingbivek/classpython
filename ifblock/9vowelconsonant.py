407862d13c29ad57e48c34b464d34b7ad5028623
beingbivek <beingbivek@gmail.com>
2024-12-17 17:17:39 +0545

    adding missing programs

---
+++ ifblock/9vowelconsonant.py
@@ -0,0 +1,6 @@
+char = input("Enter a character: ").lower()
+
+if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+    print("The character is a vowel")
+else:
+    print("The character is a consonant")
